80edef863f6c9e2c8fb333c7544dccdee6fb5919
Joachim Schiele <js@lastlog.de>
2014-02-15 19:38:01 +0100

    finally there

--- relais-tool/relais-tool.c
+++ relais-tool/relais-tool.c
@@ -7,22 +7,17 @@
 #define HOST "localhost"
 #define PORT 4223
 
-int main(int argc, char* argv[]) {
+// return -1 on errror
+// returns queried or new state if new state was set successfully
+int status_query(int r, int s) {
 	// Create IP connection
 	IPConnection ipcon;
 	ipcon_create(&ipcon);
         char* UID;
 
-        if (argc == 1) {
-            printf("state of all relays\n");
-            exit(0);
-        }
+        int ret = -1;
 
-        int r = atoi(argv[1])%2;
-        int ret = 0;
-
-
-        switch(atoi(argv[1])) {
+        switch(r) {
           case 0:        
           case 1:        
              UID = "brkA";
@@ -40,10 +35,10 @@ int main(int argc, char* argv[]) {
              UID = "brkD";
              break;
           default:
-           exit(1);
+	   fprintf(stderr, "r is limitted to r<8\n");
+           return -1;
         }
 
-        
 	// Create device object
 	DualRelay dr;
 	dual_relay_create(&dr, UID, &ipcon); 
@@ -51,31 +46,74 @@ int main(int argc, char* argv[]) {
 	// Connect to brickd
 	if(ipcon_connect(&ipcon, HOST, PORT) < 0) {
 		fprintf(stderr, "Could not connect\n");
-                return 1;
+                return -1;
 	}
 	// Don't use device before ipcon is connected
 
-	// Turn relay 1 on and relay 2 off.
         bool ret_relay0;
         bool ret_relay1;
         ret = dual_relay_get_state(&dr, &ret_relay0, &ret_relay1);
-        //if (ret != 1)
-        //        return 1;
+        if (ret != 0) {
+                fprintf(stderr, "return value of dual_relay_get_state = %i\n", ret);
+                return -1;
+        }
         
-        if (argc == 2) {
-          if (r == 0)
-            printf("%i\n", ret_relay0); 
-          else
-            printf("%i\n", ret_relay1);
+        if (s == -1) {
+          //printf("s == -1\n");
+          if (r%2 == 0) {
+            //printf("%i\n", ret_relay0); 
+            ret = ret_relay0;
+          } else {
+            //printf("%i\n", ret_relay1);
+            ret = ret_relay1;
+          }
         } else {
-          if (r == 0)
-	    ret = dual_relay_set_state(&dr, atoi(argv[2]), ret_relay1);
-          else 
-	    ret = dual_relay_set_state(&dr, ret_relay0, atoi(argv[2]));
+          //printf("s != -1\n");
+          if (r%2 == 0) {
+	    if (dual_relay_set_state(&dr, s, ret_relay1) == 0)
+              ret = s;
+          } else {
+	    if (dual_relay_set_state(&dr, ret_relay0, s) == 0)
+              ret = s;
+          }
         }
 	ipcon_destroy(&ipcon); // Calls ipcon_disconnect internally
 
-        //if (ret != 1)
-        //        return 1;
-        return 0;
+        return ret;
+}
+
+int main(int argc, char* argv[]) {
+        if (argc == 1) {
+            fprintf(stderr, "state of all relays\n");
+            int arr[8];
+            for (int i=0; i < 8; ++i) {
+               int state = status_query(i,-1);
+               if (state == -1) {
+                 fprintf(stderr, "error reading all from all relays, maybe not connected via usb?\n");
+                 return 1;
+               }
+               arr[i] = state;
+            }
+            printf("{ \"data\" : [ "); 
+            for (int i=0; i < 8; ++i) {
+               if (i > 0)
+                 printf(",");
+               printf("{\"value\" : \"%i\"}", arr[i]);
+            }
+            printf(" ] }\n");
+            return 0;
+        } else if (argc == 2 || argc == 3) {
+           int r = atoi(argv[1]);
+
+           int s = -1;
+           if (argc == 3) 
+             s = atoi(argv[2])%2;
+
+           int n = status_query(r, s);
+           if (n == 0 || n == 1) 
+             printf("%i\n", n); 
+             return 0;
+        } 
+        // error happended
+        return 1;
 }
